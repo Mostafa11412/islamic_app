c86d3d8badec0d0fd7f6b34643ec4f6575e5c989
Mostafa Saad <83826642+Mostafa11412@users.noreply.github.com>
2024-08-07 22:18:39 +0300

    ddd

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -13,7 +13,7 @@ class Homescreen extends StatefulWidget {
 }
 
 class _HomescreenState extends State<Homescreen> {
-  int index = 3;
+  int index = 3; 
   @override
   Widget build(BuildContext context) {
     return Stack(children: [
